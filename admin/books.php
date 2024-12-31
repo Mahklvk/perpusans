d463c289c890adaa41daf6caf0817b005b942f41
Mahklvk <nyehggg@gmail.com>
2024-12-31 07:07:21 +0700

    change the whole design and function

--- admin/books.php
+++ admin/books.php
@@ -1,11 +1,8 @@
 <?php
-require ('config/session.php');
-require('config/conn.php');
+require "config/session.php";
+require "config/conn.php";
 
-$queryGetBooks = mysqli_query($conn, "SELECT * FROM books");
-$jumlahBooks = mysqli_num_rows($queryGetBooks);
-
-
+$query = mysqli_query($conn, "SELECT * FROM books");
 ?>
 
 <!DOCTYPE html>
@@ -14,82 +11,90 @@ $jumlahBooks = mysqli_num_rows($queryGetBooks);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../assets/fontawesome/css/all.min.css">
-    <title>Books</title>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <style>
-        .button-add{
-            text-align: center;
-        }
-    </style>
+    <title>Books</title>
 </head>
 <body>
-    <?php include('config/navbar.php')?>
 
-    <div class="container mt-3">
-        <div class="">
-            <h1 class="text-center">List Books</h1>
-                <a href="addbooks.php"><button type="button" class="btn btn-primary button-add col-12">Add Book</button></a>
-            <div class="table-responsive mt-5">
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>No</th>
-                            <th>Judul Buku</th>
-                            <th>Author</th>
-                            <th>Category</th>
-                            <th>Created At</th>
-                            <th>Link</th>
-                            <th>Image</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        if($jumlahBooks == 0){
-                            ?>
-                            <tr>
-                                <td colspan="5" class="text-center">Data Buku Tidak ada</td>
-                            </tr>
-<?php
-                        }else{
-                            $jumlah = 1;
-                            while($data=mysqli_fetch_array($queryGetBooks)){
-                                ?>
-                                <tr>
-                                    <td><?php echo $jumlah;?></td>
-                                    <td><?php echo $data['title']?></td>
-                                    <td><?php echo $data['author']?></td>
-                                    <td><?php echo $data['category']?></td>
-                                    <td><?php echo $data['created_at']?></td>
-                                    <td><?php 
-                                    echo strlen($data['download_link']) > 25
-                                        ? htmlspecialchars(substr($data['download_link'], 0, 25)) . '...' 
-                                        : htmlspecialchars($data['download_link']); 
-                                    ?></td>
-                                    <td><?php echo $data['image']?></td>
-                                    <td>
-                                        <a href="bookdetail.php?p=<?php echo $data['book_id']; ?>">
-                                            <div class="btn btn-primary">Edit</div>
-                                        </a>
-                                    </td>
-                                    <td>
-                                    <a href="userdelete.php?p=<?php echo $data['book_id']; ?>">
-                                            <div class="btn btn-danger" >Delete</div>
-                                        </a>
-                                    </td>
-                                </tr>
-                                <?php
-                                $jumlah++;
-                            }
-                        }
-                    ?>
 
-                    </tbody>
-                </table>
-            </div>
-        </div>
+    <div class="container mt-5">
+        <h3 class="text-center">Books</h3>
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Title</th>
+                    <th>Author</th>
+                    <th>Category</th>
+                    <th>Description</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php $no = 1; while ($data = mysqli_fetch_array($query)) { ?>
+                <tr>
+                    <td><?php echo $no++; ?></td>
+                    <td><?php echo $data['title']; ?></td>
+                    <td><?php echo $data['author']; ?></td>
+                    <td><?php echo $data['category']; ?></td>
+                    <td><?php echo strlen($data['description']) > 50 ? substr($data['description'], 0, 50) . '...' : $data['description']; ?></td>
+                    <td>
+                        <a href="bookdetail.php?p=<?php echo $data['book_id']; ?>" class="btn btn-sm btn-warning">Edit</a>
+                        <button class="btn btn-sm btn-danger" onclick="deleteBook(<?php echo $data['book_id']; ?>)">Delete</button>
+                    </td>
+                </tr>
+                <?php } ?>
+            </tbody>
+        </table>
     </div>
-    <script src="../assets/bootstrap/js/bootstrap.bundle.js"></script>
-<script src="../assets/fontawesome/js/all.min.js"></script>
+
+    <script>
+        function deleteBook(bookId) {
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    fetch('delete_book.php', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify({ book_id: bookId }),
+                    })
+                    .then(response => response.json())
+                    .then(data => {
+                        if (data.success) {
+                            Swal.fire(
+                                'Deleted!',
+                                'The book has been deleted.',
+                                'success'
+                            ).then(() => location.reload());
+                        } else {
+                            Swal.fire(
+                                'Error!',
+                                data.message || 'Failed to delete the book.',
+                                'error'
+                            );
+                        }
+                    })
+                    .catch(error => {
+                        Swal.fire(
+                            'Error!',
+                            'An error occurred while processing the request.',
+                            'error'
+                        );
+                    });
+                }
+            });
+        }
+    </script>
+
+    <script src="../bootstrap/js/bootstrap.bundle.js"></script>
 </body>
 </html>
